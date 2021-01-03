2d24fcb4038ace31584cc3b394410a4d0730d30c
fenofista <67248815+fenofista@users.noreply.github.com>
2021-01-04 00:39:43 +0800

    Add files via upload

---
+++ generate.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+  int a = 31;
+
+  int b = 20;
+
+  int c ;
+
+  c = a % b; // get the remainder
+
+  printf("a: %d; b: %d; c: %d\n", a, b, c);
+
+  c=a++;
+
+  printf("a: %d; b: %d; c: %d\n", a, b, c);
+
+  c=--b;
+
+    printf("a: %d; b: %d; c: %d\n", a, b, c);
+    return 0;
+
+}
